6ad7832a8c55a29b09fee3a817d627a53218750a
dzhkaeffi <dzikajev@gmail.com>
2022-12-23 15:27:12 +0200

    update on stops

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,257 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 import Combobox from "react-widgets/Combobox";
 import "react-widgets/styles.css";
-// const proxy = 'http://localhost:5000'
-
-const stopsAPI = 'http://localhost:5000/api/bus_stops'
-const routesAPI = 'http://localhost:5000/api/bus_routes'
-const timesAPI = 'http://localhost:5000/api/bus_times'
-const tripsAPI = 'http://localhost:5000/api/bus_trips'
+import "./App.css";
+// const stopsAPI = "http://localhost:5000/api/bus_stops";
+// const routesAPI = "http://localhost:5000/api/bus_routes";
+// const timesAPI = "http://localhost:5000/api/bus_times";
+// const tripsAPI = "http://localhost:5000/api/bus_trips";
+const API_LINK = process.env.REACT_APP_API;
+const stopsAPI = `${API_LINK}/api/bus_stops`;
+const routesAPI = `${API_LINK}/api/bus_routes`;
+const timesAPI = `${API_LINK}/api/bus_times`;
+const tripsAPI = `${API_LINK}/api/bus_trips`;
 function App() {
-  const [stops, setStops] = useState([]);
-  const [routes, setRoutes] = useState([])
-  const fetchData = () => {
-    const getStopsInfo = axios.get(stopsAPI)
-    const getRoutesInfo = axios.get(routesAPI)
-    axios.all([getStopsInfo, getRoutesInfo])
-    .then(
-      axios.spread((...allData) => {
-        const stopsData = allData[0].data.bus_stops
-        const routesData = allData[1].data.bus_routes
+    const [stops, setStops] = useState([]);
+    const [routes, setRoutes] = useState([]);
+    const [times, setTimes] = useState([]);
+    const [trips, setTrips] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    const fetchData = async () => {
+        const getStopsInfo = await axios.get(stopsAPI);
+        const getRoutesInfo = await axios.get(routesAPI);
+        const getTimesInfo = await axios.get(timesAPI);
+        const getTripsInfo = await axios.get(tripsAPI);
+        axios
+            .all([getStopsInfo, getRoutesInfo, getTimesInfo, getTripsInfo])
+            .then(
+                axios.spread((...allData) => {
+                    const stopsData = allData[0].data.bus_stops;
+                    const routesData = allData[1].data.bus_routes;
+                    const timesData = allData[2].data.bus_times;
+                    const tripsData = allData[3].data.bus_trips;
 
-        // console.log(stopsDate)
-        // console.log(routesData)
-        setStops(stopsData)
-        setRoutes(routesData)
-      })
-    )
-  }
-  useEffect(()=>{
-    fetchData();
-  }, [])
-  let stop_areas = []
-  let stop_names = []
-  stops.map((stop)=>{
-    if (!stop_areas.includes(stop.stop_area) && !stop.stop_area == "") stop_areas.push(stop.stop_area)
-    // stop_names.push(`${stop.stop_name} (${stop.stop_id})`)
-  })
-  stop_areas.sort()
-  const [selectedRegion, selectRegionCombo] = useState()
-  const selectRegion = (region) => {
-    selectRegionCombo(region)
-    stops.map((stop)=>{
-      if (stop.stop_area == region) stop_names.push(`${stop.stop_name} (${stop.stop_id})`)
-    })
-    console.log(`Region selected: ${region}`)
-    console.log(stop_names)
-  }
-  return (
-    <div>
-      <Combobox
-        id='regionCombo'
-        data={stop_areas}
-        placeholder="Select region"
-        autoSelectMatches
-        onSelect={selectedRegion => selectRegion(selectedRegion)}
-      />
-        <Combobox
-        id='stopNames'
-        data={stop_names}
-        placeholder="Select stop"
-        autoSelectMatches
-      />
-      <p>{selectedRegion}</p>
-    </div>
-  )
+                    setStops(stopsData);
+                    setRoutes(routesData);
+                    setTimes(timesData);
+                    setTrips(tripsData);
+                })
+            );
+        setLoaded(true);
+    };
+    const isInitialMount = useRef(true);
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+            fetchData();
+        } else {
+            getUserLocation();
+            // Your useEffect code here to be run on update
+        }
+    });
+    let stop_areas = []; // array to fill first combobox
+    let stop_names = []; // array to fill second combobox
+    stops.map((stop) => {
+        if (!stop_areas.includes(stop.stop_area) && !stop.stop_area == "")
+            stop_areas.push(stop.stop_area);
+    });
+    const [selectedRegion, selectRegionCombo] = useState();
+    const [stopNames, setStopNames] = useState();
+    const selectRegion = (region) => {
+        selectRegionCombo(region);
+        stops.map((stop) => {
+            if (stop.stop_area == region)
+                stop_names.push(`${stop.stop_name} (${stop.stop_code})`);
+        });
+        setStopNames(stop_names.sort());
+    };
+    const [useGEO, setUseGEO] = useState("false"); // bool
+    const [userRegion, setUserRegion] = useState("undefined");
+    const [closestStop, setClosestStop] = useState("undefined");
+    let distances = [];
+    const getUserLocation = async () => {
+        navigator.geolocation.getCurrentPosition(
+            async (pos) => {
+                // Successfully got location //
+                const userLocation = {
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude,
+                };
+                // getClosestStop
+                stops.map((stop) => {
+                    if (
+                        String(stop.stop_lat).slice(0, 3) ==
+                            String(userLocation.lat).slice(0, 3) &&
+                        String(stop.stop_lon).slice(0, 5) ==
+                            String(userLocation.lng).slice(0, 5)
+                    ) {
+                        if (stop.stop_area != null) {
+                            setUserRegion(stop.stop_area);
+                        }
+                    }
+                    if (stop.stop_area == userRegion) {
+                        const distance = Math.sqrt(
+                            (userLocation.lat - stop.stop_lat) ** 2 +
+                                (userLocation.lng - stop.stop_lon) ** 2
+                        );
+                        distances.push(distance);
+                        distances.sort();
+                        if (distances[0] == distance) {
+                            setClosestStop(
+                                `${stop.stop_name} (${stop.stop_code})`
+                            );
+                        }
+                    }
+                });
+                // bool and text changes
+                setUseGEO(true);
+                const userRegionText = document.querySelector("#userRegion");
+                userRegionText.innerHTML = ` ${userRegion}`;
+                const closestStopText =
+                    document.querySelector("#userClosestStop");
+                closestStopText.innerHTML = ` ${closestStop}`;
+            },
+            () => {
+                // Location prompt is declined //
+                setUseGEO(false); // Set bool to false *used in render*
+            }
+        );
+    };
+    const [shortNames, setShortNames] = useState([]);
+    const handleConfirm = () => {
+        const selectedStop = document.getElementsByName("stopNames")[0].value;
+        let stop_code_si = selectedStop.indexOf("(") + 1;
+        let stop_code_ei = selectedStop.indexOf(")");
+        const stop_code = selectedStop.slice(stop_code_si, stop_code_ei);
+        let stop_ids = [];
+        let trip_ids = [];
+        let route_ids = [];
+        let short_names = [];
+        const stop_name = selectedStop.slice(0, stop_code_si - 2);
+        stops.forEach((stop) => {
+            if (stop.stop_name == stop_name) {
+                stop_ids.push(stop.stop_id);
+            }
+        });
+        console.log(stop_ids);
+        times.forEach((time) => {
+            stop_ids.forEach((stop_id) => {
+                if (time.stop_id == stop_id) {
+                    trip_ids.push(time.trip_id);
+                }
+            });
+        });
+        console.log(trip_ids);
+        trips.forEach((trip) => {
+            trip_ids.forEach((trip_id) => {
+                if (trip.trip_id == trip_id) {
+                    route_ids.push(trip.route_id);
+                }
+            });
+        });
+        console.log(route_ids);
+        routes.forEach((route) => {
+            route_ids.forEach((route_id) => {
+                if (route.route_id == route_id) {
+                    if (!short_names.includes(route.route_short_name))
+                        short_names.push(route.route_short_name);
+                    short_names.sort();
+                }
+            });
+        });
+        setShortNames(short_names);
+        let selectedRadioValue = document.querySelector(
+            'input[name="option"]:checked'
+        ).value;
+        if (selectedRadioValue == "time") {
+            console.log("Selected: Check the departure schedule");
+        }
+        if (selectedRadioValue == "route") {
+            console.log("Selected: Look for direct route");
+        }
+        // CODE
+        console.log(shortNames);
+    };
+    // WEB PAGE RENDER
+    return (
+        <div className="App">
+            <div className="spacergrid grid"></div>
+            {useGEO && loaded ? (
+                <div className="combobox-widgets">
+                    <div className="widget-one">
+                        <Combobox
+                            name="regionCombo"
+                            placeholder="Select region"
+                            data={stop_areas.sort()}
+                            autoSelectMatches
+                            onToggle={(selectedRegion) =>
+                                selectRegion(selectedRegion)
+                            }
+                            onSelect={(selectedRegion) =>
+                                selectRegion(selectedRegion)
+                            }
+                        />
+                    </div>
+                    <div className="widget-two">
+                        <Combobox
+                            name="stopNames"
+                            data={stopNames}
+                            placeholder="Select stop"
+                            autoSelectMatches
+                        />
+                        <button onClick={handleConfirm} className="submit">
+                            Confirm
+                        </button>
+                    </div>
+                    <div className="radio-buttons">
+                        <input
+                            className="radio-btn"
+                            type="radio"
+                            value="time"
+                            name="option"
+                            defaultChecked
+                        />{" "}
+                        Check the departure schedule
+                        <br></br>
+                        <input
+                            className="radio-btn"
+                            type="radio"
+                            value="route"
+                            name="option"
+                        />{" "}
+                        Look for direct route
+                    </div>
+                    <div className="userLocationDiv">
+                        Your current region is:
+                        <span id="userRegion"></span>
+                        <br />
+                        Closest stop to you is:
+                        <span id="userClosestStop"></span>
+                    </div>
+                    <div className="avialableBusses">
+                        Avialable Busses:
+                        <select>
+                            {shortNames.forEach((name) => {
+                                <option>a</option>;
+                            })}
+                        </select>
+                    </div>
+                </div>
+            ) : (
+                <div className="notGrantedDiv">
+                    {loaded ? <></> : <h1>Loading...</h1>}
+                    {useGEO ? (
+                        <></>
+                    ) : (
+                        <h1 className="notGranted">
+                            You have to grant access to use location!
+                        </h1>
+                    )}
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
+var cors = require('cors');
+app.use(cors())
 
 const db = new sqlite3.Database('./busstops.db', (err)=>{
   if (err) return console.error(err.message);
   console.log("Established connection to DB");
 })
-
 let bus_stops = []
 let bus_times = []
 let bus_routes = []
@@ -29,26 +30,26 @@ db.each(`select * from bus_trips`, (err, row)=> {
 })
 
 app.get("/api/bus_stops", (req,res)=>{
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+  // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.send({
       bus_stops
     })
 })
 app.get("/api/bus_trips", (req,res)=>{
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+  // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.send({
       bus_trips
     })
 })
 app.get("/api/bus_routes", (req,res)=>{
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+  // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.send({
       bus_routes
     })
 })
 app.get("/api/bus_times", (req,res)=>{
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.send({
+  // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.send({
       bus_times
     })
 })
